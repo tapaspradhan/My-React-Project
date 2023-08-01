8ed2a5d08b0e0b676731bcdc246cff7e1c051c78
Tapas Pradhan <pradhanta20@gmail.com>
2023-08-01 12:10:07 +0530

    To day I try to build a nav bar

--- Simple Project/A-Simple-Project/src/App.jsx
+++ Simple Project/A-Simple-Project/src/App.jsx
@@ -1,16 +1,13 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import './App.css'
+import NavBar from './component/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
+      <NavBar/>
     </>
   )
 }
---
+++ Simple Project/A-Simple-Project/src/component/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./navbar.css"
+
+function NavBar() {
+  return (
+    <>
+      <nav className='barStyle'>
+        <ul>
+          <li><img src="" alt="Logo" srcset="" /><a href="#">Tapas</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Photo Gallery</a></li>
+          <li><a href="#">Video</a></li>
+        </ul>
+        </nav>
+    </>
+  )
+}
+
+export default NavBar
